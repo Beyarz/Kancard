d66c79079c4fe94a93a37d2c49620996dedfbb5b
Beyar <beyar-123@hotmail.com>
2020-02-02 21:19:41 +0100

    Imported bulma as local asset

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,14 @@
 /* eslint-disable semi */
 /* eslint-disable quotes */
 
+// Bulma css
+import "bulma/css/bulma.css";
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 import "channels";
+
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
