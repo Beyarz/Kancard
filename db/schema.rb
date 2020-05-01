30d03d898a9c3a71505b54f09e3790a15c32ed6e
Beyar <beyar-123@hotmail.com>
2020-05-01 16:57:43 +0200

    Fixed invalid links when redirecting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_162455) do
+ActiveRecord::Schema.define(version: 2020_02_06_162714) do
 
   create_table "cards", force: :cascade do |t|
     t.string "title", limit: 25
