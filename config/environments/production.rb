80b2871b2f3b1b947c577c0db80e3d8f1d63540a
Beyar <beyar-123@hotmail.com>
2021-11-30 10:57:59 +0100

    Rails generate devise:install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Devise
+  # Replace the host with the domain used in production
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
