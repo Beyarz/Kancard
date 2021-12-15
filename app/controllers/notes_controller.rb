e5a0138dd2cb9f4e63122f9dd5ac4556fe65f9e5
Beyar <beyar-123@hotmail.com>
2021-12-16 00:35:49 +0100

    Using the helper method from acts_as_list instead of via Active Record

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,9 @@ class NotesController < ApplicationController
   def move
     @board = Board.find(params[:board_id])
     @note = @board.notes.find(params[:note_id])
+
+    # @note.update(position: params[:position].to_i)
+    # @note.save
     @note.insert_at(params[:position].to_i)
     head :ok
   end
