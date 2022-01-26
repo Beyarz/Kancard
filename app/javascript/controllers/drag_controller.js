e80fa60b6ce7ae9eedd7793b4556daaa944205c1
Beyar <beyar-123@hotmail.com>
2022-01-26 22:42:12 +0100

    Redesigned the layout in preparation for the chat feature

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -16,12 +16,14 @@ export default class extends Controller {
     const board_id = event.item.dataset.boardId
     const newParentId = event.to.dataset.id
 
-    // acts_as_list starts at 0 while Sortable starts at 1, so we pad the value with 1
+    // Acts_as_list starts at 0 while Sortable starts at 1, so we pad the value with 1
     const padding = 1
 
     let data = new FormData
     data.append("position", event.newIndex + padding)
     data.append("parent_column_id", newParentId)
+
+    // note.classList.toggle('is-one-quarter')
 
     Rails.ajax({
       url: this.data
