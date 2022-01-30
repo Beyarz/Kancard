5551b78aa14beb55a51c811acf9e49a467b6fcad
Beyar <beyar-123@hotmail.com>
2022-01-30 19:49:14 +0100

    Removed column "delivered" and replaced it with "created_at"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_170922) do
+ActiveRecord::Schema.define(version: 2022_01_30_182250) do
 
   create_table "boards", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2022_01_27_170922) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
-    t.datetime "delivered"
     t.integer "board_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
