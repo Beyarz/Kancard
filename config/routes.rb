8b8434752d31e9e3e934597f2f9f5341eebd9342
Beyar <beyar-123@hotmail.com>
2022-01-31 00:37:22 +0100

    Added destroy route for each messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :boards do
     resources :cards, only: [:create, :destroy]
-    resources :messages, only: [:create]
+    resources :messages, only: [:create, :destroy]
     resources :notes, only: [:create, :destroy] do
       patch :move
     end
