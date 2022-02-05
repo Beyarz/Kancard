ce51d27c7df75997d6aca1a15316728697cbe038
Beyar <beyar-123@hotmail.com>
2022-02-05 19:13:58 +0100

    Added Bullet gem to detect issues like n+1 query

--- README.md
+++ README.md
@@ -7,7 +7,13 @@
   - Bundler
   - Npm
 
-## Requirements
+## Issues / room for improvment
+
+- The owner value & owner id is being passed from the client to the server as parameter, this can easily be forged by the client in order to fool the server. Instead, the two values should be handled in the controller and injected to the BoardsController#create action.
+- Notes does not resize very well when switching between the Notes column & Cards column.
+- ./db/schema.rb lacks stronger types, some columns should not be nullable & other columns should have a default value. Other columns would be much better if it was typed as Array instead of String.
+
+## Requirements in production
 
 - Redis
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,29 @@
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+    Bullet.sentry = true
+    Bullet.bullet_logger = true
+    Bullet.growl = false
+    Bullet.honeybadger = true
+    Bullet.bugsnag = true
+    Bullet.appsignal = true
+    Bullet.airbrake = true
+    Bullet.rollbar = true
+    Bullet.skip_html_injection = false
+    # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #                 :password => 'bullets_password_for_jabber',
+    #                 :receiver => 'your_account@jabber.org',
+    #                 :show_online_status => true }
+    # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    # Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
+    # Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Devise
