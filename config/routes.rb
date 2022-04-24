e25e8750dc8fb8b454bdf3f268f90a29896267c4
Beyar <beyar-123@hotmail.com>
2022-04-24 20:20:16 +0200

    Removed unused extension

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_for :users
 
   root to: "auth#index"
   get "auth/index", to: "auth#index"
